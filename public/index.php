015db8fedeb85b517793d022a844f63c5f755c92
Jacob Christiansen <jacob@colourbox.com>
2018-08-08 12:56:20 +0200

    Added simple web app

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+$serverName = $_SERVER['SERVER_NAME'];
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Simple web app</title>
+        <meta name="description" content="The most simple web app in the world">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+
+    <body>
+        <h1>Simple web app</h1>
+        <p>BEHOLD!! This is the most simple web app in the world running at <?=$serverName?></p>
+        <img src="/images/COLOURBOX3907917.jpg" alt="Hatched kitten" height="450" width="813">
+    </body>
+</html>
